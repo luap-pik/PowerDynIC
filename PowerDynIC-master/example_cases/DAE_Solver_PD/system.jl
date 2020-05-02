da9ae4cdd0647b2b556cb9a17a691f559b79e69f
Anbue63 <36276386+Anbue63@users.noreply.github.com>
2020-05-02 22:46:39 +0100

    Delete system.jl

--- PowerDynIC-master/example_cases/DAE_Solver_PD/system.jl
+++
@@ -1,9 +0,0 @@
-line_list = []
-append!(line_list, [StaticLine(from = 1, to = 2, Y = -1im / 0.02)])
-append!(line_list, [StaticLine(from = 2, to = 3, Y = -1im / 0.02)])
-append!(line_list, [StaticLine(from = 1, to = 3, Y = -1im / 0.02)])
-
-node_list = []
-append!(node_list, [SlackAlgebraic(U = 1, Y_n = 1)])
-append!(node_list, [ThirdOrderEq(H = 3.318, P = -0.6337, D = 0.1, Ω = 50, E_f = 0.5, T_d_dash = 8.690, X_q_dash = 0.103, X_d_dash = 0.111, X_d = 0.1)])
-append!(node_list, [ThirdOrderEq(H = 3.318, P = -0.6337, D = 0.1, Ω = 50, E_f = 0.5, T_d_dash = 8.690, X_q_dash = 0.103, X_d_dash = 0.111, X_d = 0.1)])
